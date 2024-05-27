b3b7e33c4850d6292cfbf37d05d341afe30dfb30
Viver Bungag <viverbungag1@gmail.com>
2024-05-27 16:20:19 +0800

    |CHORE| initial setup

---
+++ backend/src/main/java/com/ksu/inventory_management_system_backend/InventoryManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ksu.inventory_management_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ksu/inventory_management_system_backend/InventoryManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksu.inventory_management_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
