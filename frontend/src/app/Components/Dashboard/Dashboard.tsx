97b38cb2ede8e57b4b0b4e7454c99baecd1f6d59
Patrick <patmatayabas@gmail.com>
2024-05-28 16:20:55 +0800

    Added global fonts

---
+++ frontend/src/app/Components/Dashboard/Dashboard.tsx
@@ -0,0 +1,17 @@
+export default function Dashboard(){
+  return (
+    <div>
+        <div>
+           <h1>H1: The quick brown fox jumps over the lazy dog</h1>
+           <h2>H2: The quick brown fox jumps over the lazy dog</h2>
+           <h3>H3: The quick brown fox jumps over the lazy dog</h3>
+           <h4>H4: The quick brown fox jumps over the lazy dog</h4>
+           <h5>H5: The quick brown fox jumps over the lazy dog</h5>
+           
+           <p>p: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti minus modi vero harum, quo neque. Soluta possimus amet impedit voluptatum provident, quas libero laboriosam, aliquid minima esse autem perspiciatis corrupti.</p>
+
+        </div>
+    </div>
+  )
+}
+
